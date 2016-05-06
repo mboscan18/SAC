b74624a8f0276e3ad23d48a9ba4b874f2c22c112
mboscan18 <miguelboscan18@gmail.com>
2016-05-06 09:03:18 -0400

    Pagos Terminado

--- app/Proyectos.php
+++ app/Proyectos.php
@@ -79,6 +79,7 @@ class Proyectos extends Model
         foreach ($resumenContratos as $key) {
             
             $valor_Proyecto = $valor_Proyecto + $key->valor_Contrato;
+            return $valor_Proyecto;
             $monto_Pagar_Proyecto = $monto_Pagar_Proyecto + $key->monto_Pagar_Contrato;
             $enviado_Pagar_Proyecto = $enviado_Pagar_Proyecto + $key->enviado_Pagar_Contrato;
             $faltante_Enviar_Pagar_Proyecto = $faltante_Enviar_Pagar_Proyecto + $key->faltante_Enviar_Pagar_Contrato;
