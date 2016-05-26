05a3984356fdf80b298166b049a85fb0ca617fc9
mboscan18 <miguelboscan18@gmail.com>
2016-05-26 09:14:30 -0400

    Pagos Reparado

--- app/Http/Controllers/PagosController.php
+++ app/Http/Controllers/PagosController.php
@@ -39,7 +39,7 @@ class PagosController extends Controller
         $resumenProyectos = array();
         $i = 0;
       //  foreach ($proyectos as $key) {
-            $resumenProyectos[$i] = Proyectos::reumenProyecto($proyectos[8]->id);
+            $resumenProyectos[$i] = Proyectos::reumenProyecto($proyectos[$i]->id);
        // return $resumenProyectos[$i];
             $i++;
       // }
